53e1208ae8296d418f883f58a7d13690194fa897
VrindhaAravind <86411764+VrindhaAravind@users.noreply.github.com>
2021-09-23 11:48:27 +0530

    Update comments.py

--- set5/comments.py
+++ set5/comments.py
@@ -41,43 +41,4 @@ def comment_details(postid):
             print("postId:", i['postId'], "\nid:", i['id'], '\nname:', i['name'], "\nemail:", i['email'], "\nbody",
                   i['body'], "\n")
 
-# def comment(postid):
-#     for i in comments:
-#         if i['postId']==postid:
-#             print(i)
 
-# print("Select ptions: 1. Print complete posts from all user. 2. Posts from a particular user,given the post")
-# ans=input()
-# # if ans=='Y':
-# print_posts()
-choice=input("Get details of 1.Posts, 2.Comments :")
-if choice=='1':
-    ans=input("Want to print all posts from user?")
-    if ans=='yes':
-        print_posts()
-    else:
-        ans1=input("Want post of a particular user?")
-        if ans1=='yes':
-            uid=int(input("Enter user id :"))
-            user_posts(uid)
-        else:
-            ans2=input("Want details of a post?")
-            if ans2=='yes':
-                inp = int(input("Enter post id :"))
-                post_details(inp)
-            else:
-                print("Invalid input")
-elif choice=='2':
-    ans=input("Want to print all comments?(Y/N)")
-    if ans=='Y':
-        print_comments()
-    else:
-        ans2=input("Want comments of particular post?(Y/N)")
-        if ans2=='Y':
-            inp = int(input("Enter post id :"))
-            comment_details(inp)
-        else:
-            print("invalid input")
-
-else:
-    print("Invalid Option")
